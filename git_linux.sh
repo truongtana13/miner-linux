7561de8ff435378bd5c216056c0577b35f8b71d9
Nguyen Truong Tan <truongtana13@gmail.com>
2025-02-06 04:34:12 +0700

    Update git_linux.sh

--- git_linux.sh
+++ git_linux.sh
@@ -2,4 +2,7 @@ echo $1
 cd ~/ccminer-linux
 chmod +x *
 #./ccminer-3.8.3-4_ARM -a verus -o stratum+tcp://na.luckpool.net:3960 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.V01_N01 -p x -t 8
-./ccminer-3.8.3-4_ARM -a verus -o stratum+tcp://sg.vipor.net:5040 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
+
+./ccminer-3.8.3-4_ARM -a verus -o stratum+tcp://stratum-eu.paddypool.net:9998 RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
+
+#./ccminer-3.8.3-4_ARM -a verus -o stratum+tcp://sg.vipor.net:5040 -u RDTXX2QU3cY3n5jEynguNVxGTTWHdGS77p.$1 -p x -t 8
